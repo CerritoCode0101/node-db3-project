8fb3249f4c9e0069b81b9681f8bf00189dc43a69
CerritoCode0101 <CaseyCerrito@gmail.com>
2021-02-17 13:55:52 -0600

    mvp

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,40 @@
 // scheme-model
+const { where, from } = require("../../data/db-config");
+const db = require("../../data/db-config");
+
+function find() {
+  return db.select("*").from("schemes");
+}
+
+function findById(id) {
+  return db.select("*").from("schemes").where("id", id);
+}
+
+function findSteps(id) {
+  return db
+    .select("instructions")
+    .from("steps")
+    .where("scheme_id", id)
+    .orderBy("step_number");
+}
+
+function add(scheme) {
+  return db.insert(scheme).into("schemes");
+}
+
+function update(changes, id) {
+  return db.select("*").from("schemes").where("id", id).update(changes);
+}
+
+function remove(id) {
+  return db.select("*").from("schemes").where("id", id).delete();
+}
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+};
